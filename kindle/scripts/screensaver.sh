29aa0ba62f1bc337375dfdda75c90c8e094cf7f8
Andrew McOlash <amcolash@gmail.com>
2024-01-25 17:15:15 -0500

    fix screensaver script

--- kindle/scripts/screensaver.sh
+++ kindle/scripts/screensaver.sh
@@ -4,19 +4,22 @@ if [ $(lipc-get-prop com.lab126.appmgrd activeApp) == APP_ID ]; then
   # Turn on wifi if running
   lipc-set-prop com.lab126.cmd wirelessEnable 1
 
+  # Disable screensaver
+  lipc-set-prop com.lab126.powerd preventScreenSaver 1
+
+  # From https://mobileread.com/forums/showthread.php?t=200266
+  # Always force light off if it set to 0 (anything other than 0 is ignored)
+  intensity=$(cat /sys/class/backlight/max77696-bl/brightness | grep -e '[0-9]')
+  if [ "$intensity" = "1" ]; then
+    echo -n 0 >/sys/class/backlight/max77696-bl/brightness
+  fi
+
   # echo "Running"
 else
-  # Turn off screensaver if not running
+  # Enable screensaver if not running
   lipc-set-prop com.lab126.powerd preventScreenSaver 0
 
   # echo "Not running"
-fi
-
-# From https://mobileread.com/forums/showthread.php?t=200266
-# Always force light off if it set to 0 (anything other than 0 is ignored)
-intensity=$(cat /sys/class/backlight/max77696-bl/brightness | grep -e '[0-9]')
-if [ "$intensity" = "1" ]; then
-  echo -n 0 >/sys/class/backlight/max77696-bl/brightness
 fi
 
 # Keep track of battery level over time
